e0fb41ad7d1c060d39b1372dd127d91545b7e000
ArsentyStreltsov <arsentystreltsov@gmail.com>
2024-03-28 17:40:28 +0100

    npm deploy and other stuff

--- f-pressers-game/src/App.js
+++ f-pressers-game/src/App.js
@@ -4,10 +4,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 function App() {
 
   const { unityProvider } = useUnityContext({
-    loaderUrl: "build_fpressers_24/Build/build_fpressers_24.loader.js",
-    dataUrl: "build_fpressers_24/Build/759ab52b311786e4dd1951c2624f5da2.data",
-    frameworkUrl: "build_fpressers_24/Build/cd04464fa8c418de2dc4c84d497c85f4.js",
-    codeUrl: "build_fpressers_24/Build/159acb88f36fc8af537d77942ce9a53c.wasm",
+    loaderUrl: "build/build_fpressers_24/Build/build_fpressers_24.loader.js",
+    dataUrl: "build/build_fpressers_24/Build/759ab52b311786e4dd1951c2624f5da2.data",
+    frameworkUrl: "build/build_fpressers_24/Build/cd04464fa8c418de2dc4c84d497c85f4.js",
+    codeUrl: "build/build_fpressers_24/Build/159acb88f36fc8af537d77942ce9a53c.wasm",
 
   });
 
